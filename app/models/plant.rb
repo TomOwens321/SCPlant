08253d5107177ab1adda7a8c7bdd03fc3cd00131
Tom Owens <tomo@spectralogic.com>
2015-03-06 09:58:42 -0700

    A new plant will auto-generate a Genus and Species derived from the plant name.

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,15 +1,40 @@
 class Plant < ActiveRecord::Base
   #attr_accessible :family_name
+  after_validation :set_other_params
 
   belongs_to :family
   belongs_to :genus
   belongs_to :species
   belongs_to :common_name
 
-  accepts_nested_attributes_for :family, allow_destroy: false
-
   def family_name
     self.family.nil? ? "" : self.family.name
+  end
+
+  def genus_name
+    self.genus.nil? ? "" : self.genus.name
+  end
+
+  def species_name
+    self.species.nil? ? "" : self.species.name
+  end
+
+  private
+
+  def set_other_params
+    gs = self.name.split(" ")
+    if self.genus.nil?
+      genus = Genus.where(name: gs[0]).first_or_create
+      #genus.name = gs[0]
+      self.genus = genus
+      genus.save
+    end
+    if self.species.nil?
+      species = Species.where(name: gs[1]).first_or_create
+      #species.name = gs[1]
+      self.species = species
+      species.save
+    end
   end
 
 end
