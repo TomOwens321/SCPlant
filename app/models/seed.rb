e6e268fc4db1c6769426f7fb94d12679f6f0f84a
Tom <tom@towens.com>
2015-03-07 12:33:03 -0700

    Add a simple default setter for a seed lot number.

--- app/models/seed.rb
+++ app/models/seed.rb
@@ -3,6 +3,8 @@ class Seed < ActiveRecord::Base
   belongs_to :vendor
   belongs_to :location
 
+  after_initialize :defaults
+
   def plant_name
     self.plant.nil? ? "" : self.plant.name
   end
@@ -13,5 +15,10 @@ class Seed < ActiveRecord::Base
 
   def location_name
     self.location.nil? ? "" : self.location.name
+  end
+
+  def defaults
+    myID = Seed.maximum(:id).nil? ? 1 : Seed.maximum(:id) + 1
+    self.lot ||= "SCS-%04d" % myID
   end
 end
