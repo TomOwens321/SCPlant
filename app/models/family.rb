0f2c672785942162d5e6cece72131886ab2fcc37
Tom <tom@towens.com>
2015-03-10 04:24:21 -0600

    Add validations for required fields.

--- app/models/family.rb
+++ app/models/family.rb
@@ -1,3 +1,4 @@
 class Family < ActiveRecord::Base
+  validates :name, presence: true
   has_many :plants
 end
--- app/models/genus.rb
+++ app/models/genus.rb
@@ -1,3 +1,4 @@
 class Genus < ActiveRecord::Base
+  validates :name, length: { minimum: 3 }
   has_many :plants
 end
