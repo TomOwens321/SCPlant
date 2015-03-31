9d630df9c1411050727617679b6d5843dd8db5df
Tom Owens <tomo@spectralogic.com>
2015-03-31 12:40:25 -0600

    @B2:R Add links and set thermometer server name.

--- app/helpers/thermometers_helper.rb
+++ app/helpers/thermometers_helper.rb
@@ -4,8 +4,8 @@ require 'lib/ghctl/owfuncs'
 module ThermometersHelper
 
   def temperature
-    client = Onewire.client 'ghpi'
-    therms = OwFuncs::find_thermometers 'ghpi'
+    client = Onewire.client 'towens.com'
+    therms = OwFuncs::find_thermometers 'towens.com'
     therm = Thermometer.new client, therms[0]
     therm.scale = :fahrenheit
     therm.temperature
