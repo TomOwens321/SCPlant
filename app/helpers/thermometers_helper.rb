d3af2e7b38337c1f65789041df566ed66c3e1ee7
Tom <tom@towens.com>
2015-04-03 08:12:04 -0600

    Add a second thermometer gauge for water temperature.

--- app/helpers/thermometers_helper.rb
+++ app/helpers/thermometers_helper.rb
@@ -3,10 +3,10 @@ require 'lib/ghctl/owfuncs'
 
 module ThermometersHelper
 
-  def temperature
+  def temperature(sensor = 0)
     client = Onewire.client 'ghpi'
     therms = OwFuncs::find_thermometers 'ghpi'
-    therm = Thermometer.new client, therms[0]
+    therm = Thermometer.new client, therms[sensor]
     therm.scale = :fahrenheit
     therm.temperature
   end
