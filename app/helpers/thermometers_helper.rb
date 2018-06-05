7a403527e7de802ff10954ef0fba676baef008fa
Tom Owens <tom@towens.com>
2018-06-05 13:45:03 -0600

    Minor changes for testing

--- app/helpers/thermometers_helper.rb
+++ app/helpers/thermometers_helper.rb
@@ -3,9 +3,11 @@ require 'lib/ghctl/owfuncs'
 
 module ThermometersHelper
 
+  OWHOST = 'greenhousepi'
+
   def temperature(sensor = 0)
-    client = Onewire.client 'ghpi'
-    therms = OwFuncs::find_thermometers 'ghpi'
+    client = Onewire.client OWHOST
+    therms = OwFuncs::find_thermometers OWHOST
     therm = Thermometer.new client, therms[sensor]
     therm.scale = :fahrenheit
     therm.temperature
